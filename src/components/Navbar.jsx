5e81faf79590fd766890276e7ea25caa8ddf946c
Jimmy Yakubu <jimiyaks3@gmail.com>
2024-02-29 10:37:40 +0300

    slash added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,7 +41,7 @@ export default function Navbar() {
             </li>
             
             <li className="nav-item">
-              <a href="#admin-cards" className="nav-link fw-bold">
+              <a href="/#admin-cards" className="nav-link fw-bold">
                 Levels
               </a>
             </li>
